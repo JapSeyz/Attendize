eb019289635ef1c63f7afaee3bdbc431bf891144
JapSeyz <Jesper@oestertoft.dk>
2017-02-11 19:05:28 +0100

    Scan Checkin Route

--- app/Http/Controllers/API/AppController.php
+++ app/Http/Controllers/API/AppController.php
@@ -3,6 +3,7 @@
 namespace app\Http\Controllers\API;
 
 use App\Models\Attendee;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class AppController extends ApiBaseController
@@ -38,6 +39,31 @@ class AppController extends ApiBaseController
 
         return response()->json([
             'message' => $message
+        ]);
+    }
+
+    /**
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return bool
+     */
+    public function checkin(Request $request)
+    {
+        $attendee = Attendee::where('event_id', env('CURRENT_EVENT'))->where('private_reference_number', $request->code)->first();
+
+        if($attendee->has_arrived){
+            $arrivalTime = Carbon::parse($attendee->arrival_time)->format('H:i');
+            abort(400, 'Brugeren tjekkede ind kl. '.$arrivalTime);
+        }
+
+        $attendee->has_arrived = 1;
+        $attendee->arrival_time = Carbon::now()->format('Y-m-d H:i:s');
+
+        $attendee->save();
+
+        return response()->json([
+            'message' => 'Brugeren er nu tjekket ind',
+            'id' => $attendee->id,
         ]);
     }
 }
