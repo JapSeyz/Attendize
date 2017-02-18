994aa6ef736ed3aef3d531cce7d70e55cb97fede
JapSeyz <Jesper@oestertoft.dk>
2017-02-18 16:24:51 +0100

    Add Indgang to Email on new Orders

--- app/Http/Controllers/API/AppController.php
+++ app/Http/Controllers/API/AppController.php
@@ -112,7 +112,7 @@ class AppController extends ApiBaseController
         $order->order_status_id = 1;
         $order->first_name = $user->first_name;
         $order->last_name = $user->last_name;
-        $order->email = $user->email;
+        $order->email = 'Indgang';
         $order->amount = $ticket->price;
         $order->event_id = env('CURRENT_EVENT');
         $order->is_payment_received = 1;
